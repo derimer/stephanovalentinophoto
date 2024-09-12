2703d54dd603a0a20b4bad2ac5c8aca56f08090b
derimer <jderimer@gmail.com>
2024-09-12 07:10:36 +0200

    modif portrait n&b

--- portraitn&b.php
+++ portraitn&b.php
@@ -24,12 +24,12 @@
 </nav>
 
 </nav>
-   <img class="img dj gallerie" src="./image/sourire.png" alt="photo2"width="auto"height="1000"><br>
-   <img class="img dj gallerie" src="./image/IMG_20230115_220427_197.jpg" alt="photo2"width="auto"height="1000"><br>
-   <img class="img dy gallerie" src="./image/lunettes.jpg"alt="photoy"width="auto"height="1000"><br>
+   <img class="img dj gallerie" src="./image/sourire.png" alt="photo2"><br>
+   <img class="img dj gallerie" src="./image/IMG_20230115_220427_197.jpg" alt="photo2"><br>
+   <img class="img dy gallerie" src="./image/lunettes.jpg"alt="photoy"><br>
    <img class="img dz gallerie" src="./image/DP3Q5599.B.Saby.RMCE.privas.2022.NB copie.jpg"><br>
-   <img class="img dj gallerie" src="./image/DP3Q0297.noir.jpg" alt="photo2"width="auto"height="1000"><br>
-   <img class="img dz gallerie" src="./image/matth photo nb 3.jpg" alt="photo2"width="auto"height="1000"><br>
-   <img class="img dj gallerie" src="./image/SDIM4400gavdos 3.NB.jpg" alt="photo9"width="auto"height="1000"><br>
+   <img class="img dj gallerie" src="./image/DP3Q0297.noir.jpg" alt="photo2"><br>
+   <img class="img dz gallerie" src="./image/matth photo nb 3.jpg" alt="photo2"><br>
+   <img class="img dj gallerie" src="./image/SDIM4400gavdos 3.NB.jpg" alt="photo9"><br>
 </body>
 </html>
